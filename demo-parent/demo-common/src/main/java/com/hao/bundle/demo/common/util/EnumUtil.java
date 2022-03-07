a7c79c99518e43c8b7c42d98b793a60b7f893c55
lyh <Lyinghao@126.com>
2022-03-07 14:36:15 +0800

    update

--- demo-parent/demo-common/src/main/java/com/hao/bundle/demo/common/util/EnumUtil.java
+++ demo-parent/demo-common/src/main/java/com/hao/bundle/demo/common/util/EnumUtil.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class EnumUtil {
 
@@ -25,14 +27,75 @@ public class EnumUtil {
 
             List<Map<String, Object>> res = new ArrayList<>(); // 保存结果
 
-            int ordinal = 0;
             for (T enumInstance : enumArray) {
                 Map<String, Object> map = BeanUtil.beanToMap(enumInstance);
-                map.put("ordinal", ordinal++);
                 res.add(map);
             }
 
             return res;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 根据枚举类的指定属性查找枚举的实例
+     * @param clazz 枚举类的 Class 对象
+     * @param colName 检索的属性名
+     * @param colValue 属性值
+     * @param <T> 枚举类型泛型
+     * @return 返回枚举实例
+     */
+    private static <T extends Enum<T>> T findByColumn(Class<T> clazz, String colName, Object colValue) {
+        try {
+            // 获取 jvm 针对枚举类生成的 $VALUES 属性，其为枚举数组
+            Field valuesField = clazz.getDeclaredField("$VALUES");
+            valuesField.setAccessible(true);
+            T[] enumArray = (T[]) valuesField.get(null); // 转化为对应的枚举数组
+
+            for (T enumInstance : enumArray) {
+                Map<String, Object> map = BeanUtil.beanToMap(enumInstance);
+                if (Objects.equals(map.get(colName), colValue)) {
+                    return enumInstance;
+                }
+            }
+
+            throw new IllegalArgumentException(String.format("枚举值 %s(%s) 不存在", colValue, colValue.getClass().getSimpleName()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static <T extends Enum<T>> T findByCode(Class<T> clazz, Object codeValue) {
+        return findByColumn(clazz, "code", codeValue);
+    }
+
+    /**
+     * 根据枚举类的指定属性查找枚举的实例
+     * @param clazz 枚举类的 Class 对象
+     * @param col 属性的方法引用
+     * @param colValue 属性的值
+     * @param <T> 枚举类型
+     * @param <R> 属性值类型
+     * @return 枚举实例
+     */
+    public static <T extends Enum<T>, R> T findByColumn(Class<T> clazz, Function<T, R> col, R colValue) {
+        try {
+            // 获取 jvm 针对枚举类生成的 $VALUES 属性，其为枚举数组
+            Field valuesField = clazz.getDeclaredField("$VALUES");
+            valuesField.setAccessible(true);
+            T[] enumArray = (T[]) valuesField.get(null); // 转化为对应的枚举数组
+
+            for (T enumInstance : enumArray) {
+                R apply = col.apply(enumInstance);
+                if (Objects.equals(apply, colValue)) {
+                    return enumInstance;
+                }
+            }
+
+            throw new IllegalArgumentException(String.format("枚举值 %s(%s) 不存在", colValue, colValue.getClass().getSimpleName()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
